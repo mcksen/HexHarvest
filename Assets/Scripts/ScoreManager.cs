ea34bc9975b0b660350e7b6004fd7f0ebc6edca6
mcksen <ksenniamcconnell@gmail.com>
2023-07-07 22:18:23 +0100

    Started working on Score manager and highscore

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class ScoreManager : MonoBehaviour
 {
-    private int score;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highscoreText;
+
+    private int score = 0;
+    private int highScore = 0;
+
+
+
     public void Awake()
     {
         //Change to load from the saved file when savemanager is made
-        score = 0;
+        scoreText.text = "SCORE: " + score.ToString();
+        highscoreText.text = "Highdcore: " + highScore.ToString();
+
         EventManager.onScoreIncreased += IncreaseScore;
     }
 
     public void IncreaseScore()
     {
         score += 1;
+        if (score > highScore)
+        {
+            highScore = score;
+        }
     }
 
 
